8c8e8ccee20943f9fc008ad3a6cd36d9d339f0ce
hylxluan <luanplays212004@gmail.com>
2026-06-19 00:59:21 -0300

    feat: Add de entidade de categoria à empresa

---
+++ src/main/java/br/com/vanguarderp/model/Categoria.java
@@ -0,0 +1,62 @@
+package br.com.vanguarderp.model;
+
+import java.io.Serializable;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.ConstraintMode;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.ForeignKey;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "categoria")
+@SequenceGenerator(name = "seq_categoria", 
+sequenceName = "seq_categoria", 
+allocationSize = 1, initialValue = 1)
+public class Categoria implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(generator = "seq_categoria", strategy = GenerationType.SEQUENCE)
+	private Long id;
+
+	@NotBlank(message = "Nome da categoria deve ser informado!")
+	@Column(name = "nome", nullable = false, length = 150)
+	private String nome;
+	
+	@NotNull(message = "A empresa não pode ser nula!")
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "empresa_id", 
+	nullable = false,
+	foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "empresa_fk"))
+	private Empresa empresa;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Empresa getEmpresa() {
+		return empresa;
+	}
+
+	public void setEmpresa(Empresa empresa) {
+		this.empresa = empresa;
+	}
+	
+	
+}
