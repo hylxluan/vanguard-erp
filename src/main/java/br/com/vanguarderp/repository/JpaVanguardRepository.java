2dd484556973112302282623cfb4ea580a202b94
hylxluan <luanplays212004@gmail.com>
2026-08-13 17:52:10 -0300

    feat: impl de metodos genéricos

--- src/main/java/br/com/vanguarderp/repository/JpaVanguardRepository.java
+++ src/main/java/br/com/vanguarderp/repository/JpaVanguardRepository.java
@@ -1,6 +1,7 @@
 package br.com.vanguarderp.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -13,4 +14,16 @@ public interface JpaVanguardRepository<T, ID> extends JpaRepository<T, ID> {
 	Page<T> listarPaginado(Long empresaId, Pageable pageable);
 	
 	long total(Long empresaId);
+	
+	Optional<T> buscarPorId(ID id, Long empresaId);
+	
+	List<T> buscarPorIds(Iterable<ID> ids, Long empresaId);
+	
+	boolean existePorId(ID id, Long empresaId);
+	
+	List<T> listar(Long empresaId);
+	
+	long deletarAllPorIds(Iterable<ID> ids, Long empresaId);
+	
+	long deletarAll(Long empresaId);
 }
--- src/main/java/br/com/vanguarderp/repository/JpaVanguardRepositoryImpl.java
+++ src/main/java/br/com/vanguarderp/repository/JpaVanguardRepositoryImpl.java
@@ -3,16 +3,17 @@ package br.com.vanguarderp.repository;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.domain.Sort.Order;
 import org.springframework.data.jpa.repository.support.JpaEntityInformation;
 import org.springframework.data.jpa.repository.support.SimpleJpaRepository;
 
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.Query;
 import jakarta.persistence.TypedQuery;
 
 public class JpaVanguardRepositoryImpl<T, ID extends Serializable> 
@@ -21,12 +22,14 @@ public class JpaVanguardRepositoryImpl<T, ID extends Serializable>
 	
 	private final Class<T> domainClass;
 	private final EntityManager entityManager;
+	private final boolean isMultiEmpresa;
 	
 	public JpaVanguardRepositoryImpl(Class<T> domainClass, EntityManager entityManager) {
 		super(domainClass, entityManager);
 		
 		this.domainClass = domainClass;
 		this.entityManager = entityManager;
+		this.isMultiEmpresa = possuiEmpresa();
 	}
 
 	public JpaVanguardRepositoryImpl(JpaEntityInformation<T, ?> entityInformation, EntityManager entityManager) {
@@ -34,6 +37,7 @@ public class JpaVanguardRepositoryImpl<T, ID extends Serializable>
 		
 		this.domainClass = entityInformation.getJavaType();
 		this.entityManager = entityManager;
+		this.isMultiEmpresa = possuiEmpresa();
     }
 	
 	
@@ -43,15 +47,11 @@ public class JpaVanguardRepositoryImpl<T, ID extends Serializable>
 	@Override
 	public Page<T> listarPaginado(Long empresaId, Pageable pageable) {
 		String nomeEntidade = domainClass.getSimpleName();
-		boolean possuiEmpresa = possuiEmpresa();
-		String jpql = "FROM " + nomeEntidade;
-		
-		if (possuiEmpresa) {
-			jpql += " WHERE empresa.id = : empresaId";
-		}
-		
+		String jpql = "FROM " + nomeEntidade 
+				+ (isMultiEmpresa ? " e WHERE e.empresa.id = :empresaId" : "");
+
 		if (pageable.getSort().isSorted()) {
-			jpql += " order by ";
+			jpql += " ORDER BY ";
 			
 			List<String> orders = new ArrayList<>();
 			
@@ -64,7 +64,7 @@ public class JpaVanguardRepositoryImpl<T, ID extends Serializable>
 		
 		TypedQuery<T> query = entityManager.createQuery(jpql, domainClass);
 		
-		if (possuiEmpresa) {
+		if (isMultiEmpresa) {
 			query.setParameter("empresaId", empresaId);
 		}
 		
@@ -80,20 +80,135 @@ public class JpaVanguardRepositoryImpl<T, ID extends Serializable>
 	@Override
 	public long total(Long empresaId) {
 		String nomeEntidade = domainClass.getSimpleName();
-		boolean possuiEmpresa = possuiEmpresa();
-		String jpql = "SELECT COUNT(*) FROM " + nomeEntidade;
-		
-		if (possuiEmpresa) {
-			jpql += " WHERE empresa.id = : empresaId";
-		}
+		String jpql = "SELECT COUNT(*) FROM " + nomeEntidade 
+				+ (isMultiEmpresa ? " e WHERE e.empresa.id = :empresaId" : "");
 		
 		TypedQuery<Long> query = entityManager.createQuery(jpql, Long.class);
 		
-		if (possuiEmpresa) {
+		if (isMultiEmpresa) {
 			query.setParameter("empresaId", empresaId);
 		}
 		
 		return query.getSingleResult();
+	}
+
+	
+	
+	@Override
+	public Optional<T> buscarPorId(ID id, Long empresaId) {
+		String nomeEntidade = domainClass.getSimpleName();
+		String jpql = "FROM " + nomeEntidade + " e WHERE e.id = :id"
+				+ (isMultiEmpresa ? " AND e.empresa.id = :empresaId" : "");
+		
+		TypedQuery<T> query = entityManager.createQuery(jpql, domainClass);
+		
+		if (isMultiEmpresa) {
+			query.setParameter("empresaId", empresaId);
+		}
+		
+		return query.getResultStream().findFirst();
+	}
+
+	@Override
+	public List<T> buscarPorIds(Iterable<ID> ids, Long empresaId) {
+		String nomeEntidade = domainClass.getSimpleName();
+		
+		List<ID> listaIds = new ArrayList<>();
+		ids.forEach(listaIds::add);
+		
+		if (listaIds.isEmpty()) {
+			return new ArrayList<>();
+		}
+		
+		String jpql = "FROM " + nomeEntidade + " e WHERE e.id IN :ids";
+		
+		if (isMultiEmpresa) {
+	        jpql += " AND e.empresa.id = :empresaId";
+	    }
+
+		TypedQuery<T> query = entityManager.createQuery(jpql, domainClass);
+		query.setParameter("ids", listaIds);
+		
+		if (isMultiEmpresa) {
+			query.setParameter("empresaId", empresaId);
+		}
+				
+		return query.getResultList();
+	}
+
+	@Override
+	public boolean existePorId(ID id, Long empresaId) {
+		String nomeEntidade = domainClass.getSimpleName();
+		String jpql = "SELECT 1 FROM " + nomeEntidade + " e WHERE e.id = :id"
+	            + (isMultiEmpresa ? " AND e.empresa.id = :empresaId" : "");
+		
+		TypedQuery<Integer> query = entityManager.createQuery(jpql, Integer.class);
+		query.setParameter("id", id);
+		
+		if (isMultiEmpresa) {
+			query.setParameter("empresaId", empresaId);
+		}
+		
+		query.setMaxResults(1);
+		
+		return !query.getResultList().isEmpty();
+	}
+
+	@Override
+	public List<T> listar(Long empresaId) {
+		String nomeEntidade = domainClass.getSimpleName();
+		String jpql = "FROM " + nomeEntidade + (isMultiEmpresa ? " e WHERE e.empresa.id = :empresaId" : "");
+		
+		TypedQuery<T> query = entityManager.createQuery(jpql, domainClass);
+		
+		if (isMultiEmpresa) {
+			query.setParameter("empresaId", empresaId);
+		}
+		
+		return query.getResultList();
+	}
+
+	@Override
+	public long deletarAllPorIds(Iterable<ID> ids, Long empresaId) {
+		String nomeEntidade = domainClass.getSimpleName();
+		
+		List<ID> listaIds = new ArrayList<>();
+		ids.forEach(listaIds::add);
+		
+		if (listaIds.isEmpty()) {
+			return 0;
+		}
+		
+		String jpql = "DELETE FROM " + nomeEntidade + " e WHERE e.id IN :ids";
+		
+		if (isMultiEmpresa) {
+	        jpql += " AND e.empresa.id = :empresaId";
+	    }
+		
+		Query query = entityManager.createQuery(jpql); 
+		query.setParameter("ids", listaIds);
+		
+		if (isMultiEmpresa) {
+			query.setParameter("empresaId", empresaId);
+		}
+		
+		return query.executeUpdate();
+		
+	}
+
+	@Override
+	public long deletarAll(Long empresaId) {
+		String nomeEntidade = domainClass.getSimpleName();
+		String jpql = "DELETE FROM " + nomeEntidade
+				+ (isMultiEmpresa ? " e WHERE e.empresa.id = :empresaId" : "");
+		
+		Query query = entityManager.createQuery(jpql);
+		
+		if (isMultiEmpresa) {
+			query.setParameter("empresaId", empresaId);
+		}
+		
+		return query.executeUpdate();
 	}
 
 	private boolean possuiEmpresa() {
