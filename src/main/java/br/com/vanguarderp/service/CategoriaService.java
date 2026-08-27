23176035f7254baeaefbf4e5f424e006dc81e85d
hylxluan <luanplays212004@gmail.com>
2026-08-27 00:10:47 -0300

    refactor: adds de métodos genéricos de extensão na categoria service

--- src/main/java/br/com/vanguarderp/service/CategoriaService.java
+++ src/main/java/br/com/vanguarderp/service/CategoriaService.java
@@ -1,8 +1,11 @@
 package br.com.vanguarderp.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import br.com.vanguarderp.model.Categoria;
@@ -38,6 +41,38 @@ public class CategoriaService {
 	
 	public void deleteCategoriaByIdAndEmpresa(Long id, Long idEmpresa) {
 		categoriaRepository.deleteByIdAndEmpresa(id, idEmpresa);
+	}
+	
+	Page<Categoria> listarCategoriasPaginado(Long empresaId, Pageable pageable) {
+		return categoriaRepository.listarPaginado(empresaId, pageable);
+	}
+	
+	long totalCategorias(Long empresaId) {
+		return categoriaRepository.total(empresaId);
+	}
+	
+	Optional<Categoria> buscarCategoriaPorId(Long id, Long empresaId) {
+		return categoriaRepository.buscarPorId(id, empresaId);
+	}
+	
+	List<Categoria> buscarCategoriasPorIds(Iterable<Long> ids, Long empresaId) {
+		return categoriaRepository.buscarPorIds(ids, empresaId);
+	}
+	
+	boolean existeCategoriaPorId(Long id, Long empresaId) {
+		return categoriaRepository.existePorId(id, empresaId);
+	}
+	
+	List<Categoria> listarCategorias(Long empresaId) {
+		return categoriaRepository.listar(empresaId);
+	}
+	
+	long deletarCategoriasPorIds(Iterable<Long> ids, Long empresaId) {
+		return categoriaRepository.deletarAllPorIds(ids, empresaId);
+	}
+	
+	long deletarTodasCategorias(Long empresaId) {
+		return categoriaRepository.deletarAll(empresaId);
 	}
 	
 }
